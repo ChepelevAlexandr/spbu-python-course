b28fb949f60db268d03263455a04408a0132b2e6
ChepelevAlexandr <146766199+ChepelevAlexandr@users.noreply.github.com>
2024-11-14 00:39:49 +0600

    matrix, vector operations

---
+++ project/matrix_operations.py
@@ -0,0 +1,81 @@
+
+from typing import List
+
+
+def matrix_add(m1: List[List[float]], m2: List[List[float]]) -> List[List[float]]:
+    """
+    Add two matrices.
+
+    Parameters:
+    ----------
+    A : List[List[float]]
+        The first matrix.
+    B : List[List[float]]
+        The second matrix.
+
+    Returns:
+    -------
+    List[List[float]]
+        The resulting matrix after addition.
+
+    Raises:
+    ------
+    ValueError
+        If the matrices have different dimensions.
+    """
+    if len(m1) != len(m2) or any(len(row1) != len(row2) for row1, row2 in zip(m1, m2)):
+        raise ValueError("Matrices have different dimensions!")
+
+    return [[x + y for x, y in zip(row1, row2)] for row1, row2 in zip(m1, m2)]
+
+
+def matrix_mul(m1: List[List[float]], m2: List[List[float]]) -> List[List[float]]:
+    """
+    Multiply two matrices.
+
+    Parameters:
+    ----------
+    A : List[List[float]]
+        The first matrix.
+    B : List[List[float]]
+        The second matrix.
+
+    Returns:
+    -------
+    List[List[float]]
+        The resulting matrix after multiplication.
+
+    Raises:
+    ------
+    ValueError
+        If the number of columns in the first matrix does not match the number of rows in the second matrix
+    """
+    # Check for empty matrices
+    if not m1 or not m2:
+        return []
+
+    # Check if the number of columns in the first matrix matches the number of rows in the second matrix
+    if len(m1[0]) != len(m2):
+        raise ValueError(
+            "The number of columns of the first matrix must match the number of rows of the second matrix."
+        )
+
+    # Multiply the matrices
+    return [[sum(x * y for x, y in zip(row, col)) for col in zip(*m2)] for row in m1]
+
+
+def matrix_trans(m: List[List[float]]) -> List[List[float]]:
+    """
+    Transpose a matrix.
+
+    Parameters:
+    ----------
+    A : List[List[float]]
+        The matrix to be transposed.
+
+    Returns:
+    -------
+    List[List[float]]
+        The transposed matrix.
+    """
+    return list(map(list, zip(*m)))
---
+++ project/vector_operations.py
@@ -0,0 +1,92 @@
+
+import math
+from typing import List
+
+
+def scalar(v1: List[float], v2: List[float]) -> float:
+    """
+    Calculate the scalar product of two vectors.
+
+    Parameters:
+    ----------
+    v1 : List[float]
+        The first input vector.
+    v2 : List[float]
+        The second input vector.
+
+    Returns:
+    -------
+    float
+        The scalar product of the vectors.
+
+    Raises:
+    ------
+    ValueError
+        If the lengths of the vectors are not equal.
+    """
+    if len(v1) != len(v2):
+        raise ValueError("The dimensions of the vectors must match.")
+
+    return sum(x * y for x, y in zip(v1, v2))
+
+
+def length(v: List[float]) -> float:
+    """
+    Calculate the length (magnitude) of a vector.
+
+    Parameters:
+    ----------
+    v : List[float]
+        The vector.
+
+    Returns:
+    -------
+    float
+        The length of the vector.
+
+    Raises:
+    ------
+    Exceptions:
+    ValueError: If the vector is empty.
+    """
+    if not v:
+        raise ValueError("The vector must not be empty.")
+
+    return math.sqrt(scalar(v, v))
+
+
+def angle(v1: List[float], v2: List[float]) -> float:
+    """
+    Calculate the angle between two vectors.
+
+    Parameters:
+    ----------
+    v1 : List[float]
+        The first vector.
+    v2 : List[float]
+        The second vector.
+
+    Returns:
+    -------
+    float
+        The angle between the vectors in radians.
+
+    Raises:
+    ------
+    ValueError: If the length of either vector is zero.
+
+    """
+    length_v1 = length(v1)
+    length_v2 = length(v2)
+
+    if length_v1 == 0 or length_v2 == 0:
+        raise ValueError(
+            "The length of one of the vectors is zero, the angle cannot be determined."
+        )
+
+    # Scalar multiply
+    dot_product = scalar(v1, v2)
+    cos_angle = dot_product / (length_v1 * length_v2)
+    cos_angle = max(-1.0, min(1.0, cos_angle))
+
+    return math.acos(cos_angle)
